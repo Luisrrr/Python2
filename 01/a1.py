afc13bf348e578fefc196aab9021c946c588c08d
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-08-10 13:09:16 +0200

    a1

---
+++ 01/a1.py
@@ -0,0 +1,22 @@
+print("Pipi Langstrumpf sagt: 3 mal 3 macht 6!")
+print("Pipi", "Langstrumpf", "sagt", ": 3 mal 3 macht 6!")
+print("Pipi Langstrumpf sagt: " + "3 mal 3 macht 6!")
+str = "Pipi Langstrumpf sagt: 3 mal 3 macht 6!"
+print(str)
+str = "Pipi Langstrumpf sagt: 3 mal 3 macht"
+print(str + " 6!")
+
+# "Piraten entern Schiffe." string
+# ``'c'` nichts
+# "Die wilde 13" string
+# -7 int
+# 13.9 float
+# Quatsch nichts
+# "Lewandowski schießt ein Tor für die Bayern" string
+# "a" string
+# "19" string
+# 7,5 nichts wahrscheinlich
+# 'ABC' string
+# "Integer" string
+
+# Aufgabe 3: 123 ist ein int und "123" ist ein string
