3b192f3b9ce7fe4da3e92c25794b35960582cce0
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-08-22 10:25:08 +0200

    a4 kapitel 05 fertig

--- 05/a3.py
+++ 05/a3.py
@@ -1,11 +1,18 @@
 name = input("Hallo, wie ist dein Name?\n")
 produkte = ["Äpfel", "Birnen", "Erdbeeren", "Kartoffeln", "Eier"]
+# 4 Äpfel, 1 Birne, 6 Erdbeeren, 10 Kartoffeln, 0 Eier
+mengen = [4, 1, 6, 10, 0]
 print("Wir haben heute folgende Produkte im Angebot:", produkte)
 wunsch = input("Welches Produkt willst du?\n")
+wunschIndex = produkte.index(wunsch)
+wunschMenge = int(input("Wie viel willst du davon?\n"))
 if produkte.count(wunsch) > 0:
-    print("Bitte schön, hier sind ihre", wunsch + ".")
-    preise = [2.59, 2.79, 4.29, 5.99, 2.29]
-    print("Das kostet", preise[produkte.index(wunsch)])
+    if wunschMenge <= mengen[wunschIndex]:
+        print("Bitte schön, hier sind ihre", wunsch + ".")
+        preise = [2.59, 2.79, 4.29, 5.99, 2.29]
+        print("Das kostet", preise[wunschIndex])
+    else:
+        jaNein = input("Wir haben leider nur", mengen[wunschIndex], wunsch, ". Soll ich die fehlenden", mengen[wunschIndex] - wunschMenge, "für morgen bestellen?")
 else:
     print("Schade, wir haben leider keine", wunsch + ".")
 
