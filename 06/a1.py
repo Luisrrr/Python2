1d5f29aa68a8ab67abd332870ace28ad652c6c14
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-08-24 13:19:19 +0200

    Kapitel 7 start

--- 06/a1.py
+++ 06/a1.py
@@ -1,11 +1,49 @@
-a = 10
-b = 3
-c = a + b
-c = c + 2
-c = c * b
-a = 2 * b
-b = c
-c = a + b / c
-c = b % a
-b = c ** c
-print()
+import random
+
+count = -20
+string = ""
+while count < 21:
+    string += str(count) + ", "
+    count += 1
+print("Von -20 bis 20 gezählt:", string, "...")
+
+print("100 bis 0 Countdown:\n")
+count = 100
+while count > 0:
+    print(count)
+    count -= 1
+
+print("7er Reihe:\n")
+count = 7
+while count < 100:
+    print(count)
+    count += 7
+
+print("Quadratzahlen:\n")
+count = 0
+res = 0
+while res < 1000:
+    print(res)
+    res = count * count
+    count += 1
+
+string = ""
+while count != 7:
+    count = random.randint(1, 10)
+    string += str(count) + ", "
+print("Zufallszahlen von 1 bis 10 bis 7 kommt:", string + "Ende")
+
+string = ""
+while string != "Exit":
+    string = input("Gib irgendwas ein. \"Exit\" um weiter zu gehen.\n")
+
+print("Würfeln bis zum Pasch:\n")
+num1 = 1
+num2 = 2
+while num1 != num2:
+    num1 = random.randint(1, 6)
+    num2 = random.randint(1, 6)
+    print(num1, num2)
+
+print("Alle möglichen Würfelkombinationen:\n")
+while num1 == 6 & num2 == 6:
