ec77f8a28d3bc0aedddc7182bd3859e423c9d9e2
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-08-24 12:40:14 +0200

    Sachen

--- 05/a1.py
+++ 05/a1.py
@@ -1,8 +1,8 @@
-hauptStaedte = ["Berlin", "Oslo", "Wien", "Paris", "London", "Madrid", "Rom"]  # Syntax error
+hauptStaedte = ["Berlin", "Oslo", "Wien", "Paris", "London", "Madrid", "Rom"]  # Syntax error, semantischer Fehler
 hauptStaedte.append("Bern")
 anzahl = len(hauptStaedte)
 print("Die Liste besteht aus anzahl europäischen Hauptstädten")
-print("Die fünfte Hauptstadt in der Liste ist: " + hauptStaedte[4])  # Semantischer Fehler?
+print("Die fünfte Hauptstadt in der Liste ist: " + hauptStaedte[4])
 print("Die letzte Hauptstadt in der Liste ist: " + hauptStaedte[anzahl - 1])  # Exception
 # 3.
 # Es sagt mir was falsch ist
---
+++ 06/a1.py
@@ -0,0 +1,11 @@
+a = 10
+b = 3
+c = a + b
+c = c + 2
+c = c * b
+a = 2 * b
+b = c
+c = a + b / c
+c = b % a
+b = c ** c
+print()
