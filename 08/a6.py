1af2400d95337e61ac7e4e62532454cebd2331ae
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-09-12 10:02:10 +0200

    Kapitel 08 a6 fertig

--- 08/a6.py
+++ 08/a6.py
@@ -2,6 +2,7 @@ def funktion_a():
     print("Hallo! Ich bin Funktion A!")
     funktion_b()
 
+
 def funktion_b():
     print("Hallo! Ich bin Funktion B!")
     funktion_a()
@@ -16,6 +17,9 @@ print("Ende")
 # Die Ausführung des Programms beginnt in Zeile 10
 # Danach wird die a-Funktion ausgeführt und
 # "Hallo! Ich bin Funktion A!" wird in der Konsole angezeigt
-# In Zeile 11 springt das Programm in die Funktion und die Ausführung wird fortgesetzt in Zeile 2
-# Das war die letzte Anweisung in der Funktion und die Programmausführung wird fortgesetzt in Zeile im Hauptteil \ in
-# Das war die letzte Anweisung im Hauptteil und das Programm beendet sich
+# In Zeile 11 springt das Programm in die Funktion a und die Ausführung wird fortgesetzt in Zeile 2
+# funktion_b() war die letzte Anweisung in der Funktion a und die Programmausführung wird fortgesetzt in Zeile
+# nicht im Hauptteil, sondern Zeile 6
+# Nichts war die letzte Anweisung im Hauptteil und das Programm beendet sich nicht
+
+# Das Problem ist das die Methoden recursion verursachen und irgendwann das Programm keine Lust mehr hat
