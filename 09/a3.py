fa0fd98143b5e9302382aed5d2565d2af5978853
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-09-14 12:38:37 +0200

    Kapitel 09 a3 fertig

--- 09/a3.py
+++ 09/a3.py
@@ -1,5 +1,25 @@
-password = ""
-while len(password) != 3:
-    password = input("Gib ein 3 Zeichen Passwort ein: ")
+import time
+import random
 
-# Alle symbole in einer Liste
+password = ""
+maxLength = 3
+while len(password) != maxLength:
+    password = input("Gib ein " + str(maxLength) + " Zeichen Passwort ein: ").upper()
+
+symbols = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+           "V", "W", "X", "Y", "Z", "!", "$", "%", "&", "/", "(", ")", "{", "}", "[", "]", "=", "*", "_", "-", ".",
+           "#", "+", "~", "<", ">"]
+
+start = time.time()
+
+rdStr = ""
+while rdStr != password:
+    rdStr = ""
+    for x in range(0, maxLength):
+        rdStr += symbols[random.randint(0, len(symbols) - 1)]
+    print(rdStr)
+
+end = time.time()
+print("Passwort nach", end - start, " Sekunden gecrackt")
+
+# Die Laufzeiten werden mega lang bei 4+ Zeichen
