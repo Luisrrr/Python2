da08299fab6db9e698dd87f96a8be9f44811472d
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-09-21 12:57:12 +0200

    Kapitel 09 fertig fast

--- 09/a5.py
+++ 09/a5.py
@@ -7,8 +7,70 @@ def draw_board():
             str = ""
 
 
-board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-playerNum = 1
-playerChars = ["X", "O"]
+def is_winner(char):
+    # Horizontal
+    if board[0] == char and board[1] == char and board[2] == char:
+        return True
+    if board[3] == char and board[4] == char and board[5] == char:
+        return True
+    if board[6] == char and board[7] == char and board[8] == char:
+        return True
+    # Vertical
+    if board[0] == char and board[3] == char and board[6] == char:
+        return True
+    if board[1] == char and board[4] == char and board[7] == char:
+        return True
+    if board[2] == char and board[5] == char and board[8] == char:
+        return True
+    # Diagonal
+    if board[0] == char and board[4] == char and board[8] == char:
+        return True
+    if board[2] == char and board[4] == char and board[6] == char:
+        return True
 
-draw_board()
+    return False
+
+
+def board_contains_number():
+    for char in board:
+        if char.isnumeric():
+            return True
+    return False
+
+
+playerChars = ["X", "O"]
+playerNum = 1
+
+while True:
+
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    gameEnded = False
+
+    draw_board()
+
+    while gameEnded == False:
+        num = 0
+        while True:
+            num = int(input("Spieler " + str(playerNum) + " (" + playerChars[playerNum - 1] + ") ist dran:"))
+            if board[num - 1].isnumeric() == True:
+                break
+
+        board[num - 1] = playerChars[playerNum - 1]
+
+        print("\n")
+        draw_board()
+
+        if is_winner(playerChars[playerNum - 1]):
+            print("Spieler", playerNum, "(" + playerChars[playerNum - 1] + ") hat gewonnen!")
+            gameEnded = True
+        elif board_contains_number() == False:
+            print("Unentschieden!")
+            gameEnded = True
+
+        if playerNum == 1:
+            playerNum = 2
+        else:
+            playerNum = 1
+
+    if input("Nochmal spielen? (y/n) ") != "y":
+        break
---
+++ generators.py
@@ -0,0 +1,28 @@
+generators = [1, 1, 0, 0, 0, 0, 0, 0]
+mults = [1, 1, 1, 1, 1, 1, 1, 1]
+number = 0
+
+while True:
+    for c in range(0, 7):
+        generators[c] += generators[c + 1] * mults[c]
+
+    number += generators[0] * mults[0]
+
+    print()
+    print("Number:", number)
+    print("Generators:", generators)
+    print("Multipliers:", mults)
+
+    print()
+    print("(1) Increase generator")
+    print("(2) Increase multiplier")
+
+    action = input("Enter for next tick\n")
+    if action == "1":
+        index = int(input("Generator index: "))
+        amount = float(input("Amount: "))
+        generators[index] += amount
+    elif action == "2":
+        index = int(input("Multiplier index: "))
+        amount = float(input("Amount: "))
+        mults[index] += amount
