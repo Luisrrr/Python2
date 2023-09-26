e44bd686da337ce19da65b3484d5a45430141c31
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-09-26 10:20:03 +0200

    D02 scheiße

--- D2/a1_verkauf.py
+++ D2/a1_verkauf.py
@@ -1,1 +1,17 @@
+def GetFromCSV(line):
+    strs = []
+    str = ""
+    strs.append()
+    strs.append(str)
+    return strs
 
+
+file = open("hofladen.csv", "r")
+lines = file.readlines()
+
+produkte = []
+for line in lines:
+    produkte.append(line.strip().split("t"))
+
+print(lines[0])
+print(GetFromCSV(lines[0]))
