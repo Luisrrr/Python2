008e933fed7650d84ad650b115523905168a12b0
luirow050308 <luirow050308@gymnasiumhueck.onmicrosoft.com>
2023-11-09 13:21:04 +0100

    Sand

--- Ballin.py
+++ Ballin.py
@@ -3,24 +3,27 @@ import random
 
 
 class Ball:
-    x_pos = 15
-    y_pos = 3  # 0 = top
+    x_pos = 0
+    y_pos = 0  # 0 = top
     x_vel = 0
     y_vel = 0
     mass = 1
     bounciness = 0.6
     friction = 1.2
     char = "O"
-    respawn = False  # Respawns when it is almost static
+    respawn = False  # Respawns when it is close to static
 
     def __init__(self, _respawn = False):
-        self.x_vel = random.random() * 6 - 3
-        self.y_vel = random.random() * 6 - 3
+        # Spawn at center
+        self.x_pos = window_width / 2
+        self.y_pos = window_height / 2
+
+        self.apply_force(random.random() * 6 - 3, random.random() * 6 - 3)
         self.respawn = _respawn
 
     def apply_force(self, force_x, force_y):
-        self.x_vel = force_x * (1 / self.mass)
-        self.y_vel = force_y * (1 / self.mass)
+        self.x_vel = force_x / self.mass
+        self.y_vel = force_y / self.mass
 
     def simulate(self):
         window[int(self.y_pos)][int(self.x_pos)] = " "
@@ -34,14 +37,16 @@ class Ball:
 
     def check_bounds(self):
         if self.x_pos < 0:
+            # Hit left wall
             self.x_pos = 0
             self.x_vel = -1 * self.x_vel * self.bounciness
         elif self.x_pos >= window_width:
+            # Hit right wall
             self.x_pos = window_width - 1
             self.x_vel = -1 * self.x_vel * self.bounciness
 
         if self.y_pos >= window_height:
-            # Ball hit floor
+            # Hit floor
             self.y_pos = window_height - 1
             self.y_vel = -1 * math.fabs(self.y_vel) * self.bounciness
             if self.y_vel > -gravity * 15:
@@ -50,7 +55,7 @@ class Ball:
                     self.__init__()
             self.x_vel /= self.friction
         elif self.y_pos < 0:
-            # Ball hit ceiling
+            # Hit ceiling
             self.y_pos = 0
             self.y_vel = math.fabs(self.y_vel)
 
---
+++ Pharaos_Curse.py
@@ -0,0 +1,82 @@
+import math
+
+
+class Particle:
+    x_pos = 0
+    y_pos = 0
+    # Moves:
+    # 7 8 9
+    # 6 5 4
+    # 1 2 3
+    moves = []
+    x_steps = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+    y_steps = [1, 1, 1, 0, 0, 0, -1, -1, -1]
+    char = "#"
+
+    def __init__(self, _char):
+        self.x_pos = cursor_x
+        self.y_pos = cursor_y
+        self.char = _char
+        if self.char == "#":
+            self.moves = [2, 1, 3]
+        elif self.char == "O":
+            self.moves = [2, 1, 3, 6, 4]
+
+    def simulate(self):
+        for move in self.moves:
+            if self.move(self.x_steps[move - 1], self.y_steps[move - 1]):
+                break
+
+    def move(self, x, y):
+        if window[self.y_pos + y][self.x_pos + x] != " " or self.is_out_of_bounds(self.x_pos + x, self.y_pos + y):
+            return False
+
+        window[self.y_pos][self.x_pos] = " "
+        self.x_pos += x
+        self.y_pos += y
+
+        window[self.y_pos][self.x_pos] = self.char
+        return True
+
+    def is_out_of_bounds(self, x, y):
+        return x < 0 or x >= window_width - 1 or y < 0 or y >= window_height - 1
+
+
+def draw_window():
+    print("-" * (window_width + 2))
+    for y in range(window_height):
+        line = "|"
+        for x in range(window_width):
+            if x == cursor_x and y == cursor_y:
+                line += "X"
+            else:
+                line += window[y][x]
+        print(line + "|")
+    print("-" * (window_width + 2))
+
+
+window_width = 35
+window_height = 8
+window = [[" " for x in range(window_width)] for y in range(window_height)]  # Resets window
+cursor_x = int(window_width / 2)
+cursor_y = int(window_height / 2)
+tick_speed = 5  # x frames between the ticks
+
+particles = []
+current_particle = "#"
+while True:
+    for particle in particles:
+        particle.simulate()
+    draw_window()
+
+    action = input("Enter: Next tick | M: Move cursor | P: Place | S: Switch particle ").upper()
+    if action == "M":
+        move_by = input("x y: ")
+        splits = move_by.split(" ")
+        cursor_x += int(splits[0])
+        cursor_y += int(splits[1])
+    elif action == "P":
+        particles.append(Particle(current_particle))
+    elif action == "S":
+        selection = input("Sand: #  Water: O  Wall: =")
+        current_particle = selection
